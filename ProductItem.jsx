bbad16e7cc68ec48744606277254c94a058da7d9
Tiago Farinas <tiagofarinas@gmail.com>
2024-11-17 14:14:00 -0500

    Create ProductItem component to display individual product details

---
+++ ProductItem.jsx
@@ -0,0 +1,10 @@
+//Task 4 - Create the ProductItem Component
+import React from 'react';
+function ProductItem({ product }) {
+  return (
+    <div>
+      <h3>{product.name}</h3>
+      <p>Price: {product.price}</p>
+      <p>{product.description}</p>
+    </div>)};
+export default ProductItem;
